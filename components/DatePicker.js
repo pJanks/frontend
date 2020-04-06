28a7ef4ca50d1fa52f3e94cc8559690b7c0c0196
Grayson Palmer <gpalmer1385@gmail.com>
2020-04-06 12:55:11 -0600

    Remove unnecessary comment

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import DatePicker from 'react-native-datepicker'
- 
+
 export default class MyDatePicker extends Component {
   constructor(props){
     super(props)
     this.state = {date:"2020-04-01"}
   }
- 
+
   render(){
     return (
       <DatePicker
@@ -29,7 +29,6 @@ export default class MyDatePicker extends Component {
           dateInput: {
             marginLeft: 36
           }
-          // ... You can check the source to find the other keys.
         }}
         onDateChange={(date) => {this.setState({date: date})}}
       />
