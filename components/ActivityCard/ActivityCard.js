c29d5cf7a1d05e37cf95696ad0ada54432060667
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-09 12:29:35 -0600

    Get necessary data on screen

--- components/ActivityCard/ActivityCard.js
+++ components/ActivityCard/ActivityCard.js
@@ -1,22 +1,32 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-export default function ActivityCard({ date }) {
-  console.log(date);
+export default function ActivityCard({ date, location, activity, forecast }) {
   return (
     <View>
-      <Text>{date}</Text>
-      <Text>hi</Text>
+      <Text style={ styles.header }>Activity:</Text>
+      <Text>Scheduled on: {date}</Text>
+      <Text>{ activity } at: { location }</Text>
+      <Text>Forecast: { forecast }</Text>
+
     </View>
   );
 }
 
-// const styles = StyleSheet.create({
-//   picker: {
-//     height: 40,
-//     width: 250,
-//     borderColor: 'gray',
-//     borderWidth: 1,
-//     backgroundColor: '#fff',
-//   }
-// });
+const styles = StyleSheet.create({
+  activityCard: {
+    height: 80,
+    margin: 5,
+    padding: 5,
+    width: 260,
+
+  },
+
+  header: {
+    height: 40,
+    width: 250,
+    borderColor: 'gray',
+    borderWidth: 1,
+    backgroundColor: '#fff',
+  }
+});
