4ea4217f01c92f79df8b990e9c61e18d61b1b33d
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-09 13:10:03 -0600

    Adjust data and commit to run command for fixing errors with ScrollView

--- components/UserLandingPage/UserLandingPage.js
+++ components/UserLandingPage/UserLandingPage.js
@@ -1,13 +1,22 @@
 import React from "react";
 import mockData from "../../mockData/mockData";
 import ActivityCard from "../ActivityCard/ActivityCard";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, ScrollView } from "react-native";
 
 export default function UserLandingPage() {
   console.log(mockData);
   let userActivities = mockData.map(activity => {
-    return <ActivityCard date={ activity.date } location={ activity.location } activity={ activity.activity.name } forecast={ activity.forecast } />
-  })
+    return (
+      <View>
+        <ScrollView>
+          <ActivityCard
+            date={ activity.date }
+            location={ activity.location }
+            activity={ activity.activity.name }
+            forecast={ activity.forecast } />
+        </ScrollView>
+      </View>
+  )})
   return (
     <View>
       { userActivities }
