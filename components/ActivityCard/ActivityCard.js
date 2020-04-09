9b42fb256423f17bfa8b00fc9e3613bf43087e90
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-09 13:09:52 -0600

    Adjust data and commit to run command for fixing errors with ScrollView

--- components/ActivityCard/ActivityCard.js
+++ components/ActivityCard/ActivityCard.js
@@ -1,30 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 export default function ActivityCard({ date, location, activity, forecast }) {
   return (
-    <View>
-      <Text style={ styles.header }>Activity:</Text>
+    <View style={ styles.activityCard }>
+      <Text>Activity:</Text>
       <Text>Scheduled on: {date}</Text>
-      <Text>{ activity } at: { location }</Text>
+      <Text>{ activity } at/in: { location }</Text>
       <Text>Forecast: { forecast }</Text>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   activityCard: {
-    height: 80,
-    margin: 5,
-    padding: 5,
+    borderWidth: 4,
+    borderColor: "#000",
+    borderRadius: 6,
+    height: 150,
+    margin: 25,
+    padding: 15,
     width: 260,
 
   },
 
   header: {
-    height: 40,
-    width: 250,
     borderColor: 'gray',
     borderWidth: 1,
     backgroundColor: '#fff',
