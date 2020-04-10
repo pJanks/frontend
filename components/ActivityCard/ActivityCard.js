1e13d8951a14140b57dc5739c753d53b6c8d48ed
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-09 18:06:55 -0600

    Add initial styling and format screen scroll

--- components/ActivityCard/ActivityCard.js
+++ components/ActivityCard/ActivityCard.js
@@ -1,32 +1,50 @@
 import React from "react";
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 export default function ActivityCard({ date, location, activity, forecast }) {
   return (
     <View style={ styles.activityCard }>
-      <Text>Activity:</Text>
-      <Text>Scheduled on: {date}</Text>
-      <Text>{ activity } at/in: { location }</Text>
-      <Text>Forecast: { forecast }</Text>
+      <Text style={ styles.header }>Scheduled Activity:</Text>
+      <Text style={ styles.allActivityCardText }>
+        <Text style={ styles.label } >Scheduled on: </Text>
+        <Text>{ `${date}\n`}</Text>
+        <Text style={ styles.label } >{ activity } at/in: </Text>
+        <Text>{ `${location}\n`}</Text>
+        <Text style={ styles.label } >Forecast: </Text>
+        <Text>{ `${forecast}`}</Text>
+      </Text>
+      <Button style={ styles.buttonStyles } title='View Activity'></Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   activityCard: {
-    borderWidth: 4,
+    borderWidth: 2,
     borderColor: "#000",
-    borderRadius: 6,
-    height: 150,
+    borderRadius: 4,
+    height: 195,
     margin: 25,
     padding: 15,
-    width: 260,
+    width: '86%',
 
   },
 
+  allActivityCardText: {
+    fontSize: 20
+  },
+
+  buttonStyles: {
+  },
+
   header: {
-    borderColor: 'gray',
-    borderWidth: 1,
-    backgroundColor: '#fff',
+    fontSize: 25,
+    marginBottom: 10,
+    paddingHorizontal: '7%',
+    textDecorationLine: 'underline'
+  },
+
+  label: {
+    fontWeight: 'bold'
   }
 });
