9e5c00ea6f948f96dd905321151fb36618429dfe
Grayson Palmer <gpalmer1385@gmail.com>
2020-04-12 10:24:45 -0600

    Implement global hook in prep for using API data

--- App.js
+++ App.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { ActivityContext } from './context/ActivityContext';
+import { mockData } from './mockData/mockData';
 import NewActivityForm from './screens/NewActivityForm/NewActivityForm';
 import LoginScreen from './screens/LoginScreen/LoginScreen';
 import HomeScreen from './screens/HomeScreen/HomeScreen';
 import UserActivityScreen from './screens/UserActivityScreen/UserActivityScreen';
+import ActivityProvider from './provider/ActivityProvider';
 
 const Stack = createStackNavigator();
 
+
+
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='Login' component={LoginScreen} />
-        <Stack.Screen name='New Activity' component={NewActivityForm} />
-        <Stack.Screen name='UserActivityScreen' component={UserActivityScreen} />
-        <Stack.Screen name='Home' component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+    return (
+    <ActivityProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Login'>
+            <Stack.Screen name='Login' component={LoginScreen} />
+            <Stack.Screen name='New Activity' component={NewActivityForm} />
+            <Stack.Screen name='UserActivityScreen' component={UserActivityScreen} />
+            <Stack.Screen name='Home' component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+    </ActivityProvider>
+    )
+  }
 
 const styles = StyleSheet.create({
   container: {
