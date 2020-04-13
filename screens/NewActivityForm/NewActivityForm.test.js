de79d45b554d5c345af1f9d15ee888b14aaa3914
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-13 16:24:40 -0600

    Add placeholder test for CI

---
+++ screens/NewActivityForm/NewActivityForm.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import NewActivityForm from './NewActivityForm';
+
+import renderer from 'react-test-renderer';
+
+test('testing suite runs', () => {
+  expect(true).toEqual(true)
+})
