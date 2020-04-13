5fabcf75a71b56e4b597ba38995877bfd0c08f3c
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-13 16:27:27 -0600

    Add placeholder test

---
+++ screens/UserActivityScreen/UserActivityScreen.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserActivityScreen from './UserActivityScreen';
+
+import renderer from 'react-test-renderer';
+
+test('testing suite runs', () => {
+  expect(true).toEqual(true)
+})
