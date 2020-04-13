18a30f87c13446c75d81f2e1a645f0a30e3067af
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-13 16:26:48 -0600

    Add placeholder test

---
+++ screens/ActivityDetails/ActivityDetails.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ActivityDetails from './ActivityDetails';
+
+import renderer from 'react-test-renderer';
+
+test('testing suite runs', () => {
+  expect(true).toEqual(true)
+})
