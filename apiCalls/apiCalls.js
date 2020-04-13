3c4080e39a0184b018c1a8e52487656cbe92d3fa
Grayson Palmer <gpalmer1385@gmail.com>
2020-04-13 15:53:11 -0600

    Create Activity Form Post and Scheduled Activity fetch calls

--- apiCalls/apiCalls.js
+++ apiCalls/apiCalls.js
@@ -8,3 +8,36 @@ export const fetchAllUserActivities = async (userId) => {
       }
       return response.json()})
 }
+
+export const postNewActivity = async ({userId, activity, location, date}) => {
+  const url = `/api/v1/users/${userId}/scheduled_activities/new`;
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      activity,
+      location,
+      date
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  return await fetch(url, options)
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Failed post new activity.')
+          }
+          return res.json()
+        })
+}
+
+export const fetchScheduledActivity = async (userId, activityId) => {
+  const url = `https://rain-or-shine-backend.herokuapp.com/api/v1/${userId}/scheduled_activities/${activityId}`;
+  return await fetch(url)
+    .then(response => {
+      if(!response.ok) {
+        console.log(response)
+        throw Error('Failed to retrieve user activities.')
+      }
+      return response.json()})
+}
