586fae6b237819059361a7c4ffe69b674fca1c3b
Grayson Palmer <gpalmer1385@gmail.com>
2020-04-14 16:28:02 -0600

    Create mock for Skycons

--- components/WeatherWindow/WeatherWindow.test.js
+++ components/WeatherWindow/WeatherWindow.test.js
@@ -3,6 +3,16 @@ import WeatherWindow from './WeatherWindow';
 
 import renderer from 'react-test-renderer';
 
+jest.mock("react-skycons", () => {
+  return function DummyWeatherIcon() {
+    return (
+      <div data-testid="skycon">
+        mock skycon
+      </div>
+    );
+  };
+});
+
 test('renders correctly', () => {
 
   const weatherWindowComponent = renderer.create(<WeatherWindow />).toJSON();
