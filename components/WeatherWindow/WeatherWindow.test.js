0e115dbf9deb98c326ef0c52b4c6ef976c6b9bee
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2020-04-15 13:13:50 -0600

    Adjust tests to pass

--- components/WeatherWindow/WeatherWindow.test.js
+++ components/WeatherWindow/WeatherWindow.test.js
@@ -3,19 +3,22 @@ import WeatherWindow from './WeatherWindow';
 
 import renderer from 'react-test-renderer';
 
-jest.mock("react-skycons", () => {
-  return function DummyWeatherIcon() {
-    return (
-      <div data-testid="skycon">
-        mock skycon
-      </div>
-    );
-  };
-});
-
 test('renders correctly', () => {
 
-  const weatherWindowComponent = renderer.create(<WeatherWindow />).toJSON();
+  const activity = {
+    "id": 4,
+    "status": "bad",
+    "date": "2020-04-21T00:00:00Z",
+    "location": "Breckenridge, CO",
+    "forecast": "Foggy until afternoon, starting again overnight.",
+    "forecast_img": "rain",
+    "temperature": "45.03",
+    "temp_hi": "46.61",
+    "temp_low": "28.48",
+    "precip_probability": "0.46",
+  }
+
+  const weatherWindowComponent = renderer.create(<WeatherWindow activity={ activity } />).toJSON();
 
   expect(weatherWindowComponent).toMatchSnapshot();
 });
