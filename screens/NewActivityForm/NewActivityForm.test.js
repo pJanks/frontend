3f6d74d65d5e5e85c089d11839bb2b4afdacbffc
Grayson Palmer <gpalmer1385@gmail.com>
2020-04-15 13:29:25 -0600

    Create snapshot tests

--- screens/NewActivityForm/NewActivityForm.test.js
+++ screens/NewActivityForm/NewActivityForm.test.js
@@ -3,6 +3,18 @@ import NewActivityForm from './NewActivityForm';
 
 import renderer from 'react-test-renderer';
 
-test('testing suite runs', () => {
-  expect(true).toEqual(true)
+describe('NewActivityForm', () => {
+  test('should load with props', () => {
+    const mockRouteInputs = jest.fn().mockImplementation(() => {
+      return {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Smith'
+      }
+    })
+    const mockNavigation = { navigate: jest.fn() };
+    const mockRoute = {params: mockRouteInputs};
+    const newActivityForm = renderer.create(<NewActivityForm navigation={mockNavigation} route={mockRoute} />)
+    expect(newActivityForm).toMatchSnapshot();
+  });
 })
